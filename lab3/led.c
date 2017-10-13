e9dae6a02493732370e9e6f831f4b77b94d82e03
poyuan <pyjeng.eed03g@nctu.edu.tw>
2017-10-13 13:09:59 +0800

    add lab3 led test.

---
+++ lab3/led.c
@@ -0,0 +1,24 @@
+#include "asm−arm/arch−pxa/lib/creator_pxa270_lcd.h"
+#include <stdlib.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+int main(){
+    int fd;
+    int retval ;
+    unsigned short data ;
+
+    if((fd = open("/dev/lcd", "O_RDWR")) < 0){
+        printf("open faled\n") ;
+        exit(-1) ;
+    }
+
+    data=LED_ALL_ON ;
+    ioctl (fd , LED_IOCTL_SET, &data );
+    printf(”Turn on all LED lamps\n”);
+    sleep (3);
+
+    close(fd) ;
+}
+
