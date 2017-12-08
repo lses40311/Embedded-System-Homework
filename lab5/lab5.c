4d1d6d2864330a205eee929328730b427d32a305
poyuan <pyjeng.eed03g@nctu.edu.tw>
2017-12-08 12:38:27 +0800

    v0.1

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -8,6 +8,8 @@
 #include <pthread.h>
 #include <signal.h>
 #include <sys/sem.h>
+#include "seg_show_number.h"
+
 
 #define PXA 0
 #define newUBER 0
@@ -23,6 +25,7 @@
 typedef struct link_list{
     int data[4] ;
     int priority ;
+    int id ;
     struct  link_list * next ;
 } link_list ;
 //Link list structure
@@ -40,6 +43,7 @@ int my_sem_init() ;
 int my_sem_remove() ;
 int my_sem_post() ;
 int my_sem_wait() ;
+void blink() ;
 
 // Global Variables
 link_list * head_request = NULL ;
@@ -48,16 +52,21 @@ pthread_mutex_t lock, navigate_sig;
 int * input_data ;
 link_list * paired_data ;
 int lcd_fd ;
-int request_sem, uber_sem ; // originally sem_t
+int request_sem, uber_sem, intersection_sem ; // originally sem_t
+int driving_thread_num ;
 
 
 // Handles Ctrl + c
 void intHandler(int dummy) {
     my_sem_remove(request_sem, 1) ;
     my_sem_remove(uber_sem, 1) ;
+    my_sem_remove(intersection_sem, MAX_AXIS_X*MAX_AXIS_Y) ;
     exit(0) ;
 }
 
+int trans_position(int x, int y){
+    return (x-1) * 9 + (y-1) ;
+}
 
 int main(int argc, char ** argv){
     if(argc != 2){
@@ -73,9 +82,21 @@ int main(int argc, char ** argv){
     // Initialize Semaphore & mutex
     //sem_init(&request_sem, 0, 0); // another way to open a semaphore
     if((request_sem = my_sem_init(MY_KEY, 1, 0)) < 0){
-        printf("%s\n", "open sem error") ;
+        printf("%s\n", "open request_sem error") ;
         exit(1) ;
     }
+    else{
+        printf("%s\n", "Open request_sem.") ;
+    }
+    if((intersection_sem = my_sem_init(MY_KEY+2, MAX_AXIS_X*MAX_AXIS_Y, 1)) < 0){
+        printf("%s\n", "open intersection_sem error") ;
+        exit(1) ;
+    }
+    else{
+        printf("%s\n", "Open intersection_sem.") ;
+    }
+
+
     pthread_mutex_init(&lock, NULL) ;
     pthread_mutex_init(&navigate_sig, NULL) ;
     pthread_mutex_lock(&navigate_sig) ;
@@ -87,8 +108,7 @@ int main(int argc, char ** argv){
         printf("open failed\n") ;
         exit(-1) ;
     }
-    ioctl(lcd_fd, KEY_IOCTL_CLEAR, key) ;
-    ioctl(lcd_fd, LCD_IOCTL_CLEAR, NULL) ;
+    ioctl(lcd_fd, _7SEG_IOCTL_ON, NULL) ;
     printf("LCD Initialization Success.\n") ;
     #endif
 
@@ -98,12 +118,14 @@ int main(int argc, char ** argv){
     pthread_create(&thread_handles[1], NULL, uber_thread, (void*) uber_loc_file );
 
     // Create a navigation thread
+    driving_thread_num = 0 ;
     while(1){
         pthread_mutex_lock(&navigate_sig) ;
         printf("Create driving thread\n") ;
         link_list * args = malloc(2 * sizeof(link_list));
         memcpy(args, paired_data, 2*sizeof(link_list)) ;
         if (pthread_create(&thread_handles[3], NULL, driving_thread, args)){
+            driving_thread_num ++ ;
             free(args) ;
         }
         pthread_detach(thread_handles[3]) ;
@@ -117,39 +139,41 @@ int main(int argc, char ** argv){
 }
 
 void * customer_thread(){
-    int * requests = malloc(4*sizeof(int)) ;
-    unsigned short key = 0 ;
     int priority = 0 ;
     while (1) {
         printf("Please Enter 4 numbers as the customer's request..\n") ;
-        int i = 0;
-        for(i =0; i<4; i++){
-            #if PXA
-            ioctl(lcd_fd, KEY_IOCTL_WAIT_CHAR, &key) ;
-            key = ((key&0xff)-48) ;
-            #else
-            scanf("%hu", &key) ;
-            #endif
-            requests[i] = (int)key ;
-			//printf("==%hu\n", key&0xff) ;
-        	printf("read %d\n", requests[i]) ;
+        int ** requests ;
+        requests = malloc(10*sizeof(int*)) ;
+        int cnt = 0 ;
+        int i ;
+        while(1){
+            requests[cnt] = malloc(4*sizeof(int)) ;
+            scanf("%d %d %d %d", &requests[cnt][0], &requests[cnt][1], &requests[cnt][2], &requests[cnt][3]) ;
+            if(requests[cnt][0] == -1){
+                break ;
+            }
+            cnt++ ;
+        }
+        for(i=0; i<cnt; i++){
+            enqueue_safe(&head_request, requests[i], priority, 0);
+            free(requests[i]) ;
+            my_sem_post(request_sem, 0);
 		}
-        enqueue_safe(&head_request, requests, priority);
-        my_sem_post(request_sem, 0);
     }
 
 }
 
 void * uber_thread(void * arg){
     char *val_p = (char *) arg ;
-    unsigned int uber_cnt = 0 ;
+    int uber_cnt = 0 ;
     int * coordinates = malloc(4*sizeof(int));
     int i ;
     for(i=0; i<4; i++) coordinates[i] = 0 ;
     FILE * fp = fopen(val_p, "r") ;
     while(fscanf(fp, "%d %d\n", &coordinates[0], &coordinates[1]) > 0){
         printf("val=%d,%d\n", coordinates[0], coordinates[1]) ;
-        head_uber = enqueue(head_uber, coordinates, 0);
+        head_uber = enqueue(head_uber, coordinates, 0, uber_cnt);
+        my_sem_wait(intersection_sem, trans_position(coordinates[0], coordinates[1]), 0) ;
 		uber_cnt++ ;
 	}
 
@@ -161,10 +185,8 @@ void * uber_thread(void * arg){
     }
 
     while (1) {
-        //sem_wait(&request_sem) ;
-        //sem_wait(&uber_sem) ;
-        my_sem_wait(request_sem, 0);
-        my_sem_wait(uber_sem, 0);
+        my_sem_wait(request_sem, 0, 0);
+        my_sem_wait(uber_sem, 0, 0);
         pthread_mutex_lock(&lock);
         printf("%s\n", "Find a car.");
         head_uber = dequeue(head_uber, paired_data, 0) ;
@@ -178,37 +200,64 @@ void * uber_thread(void * arg){
 void * driving_thread(void * args){
     link_list uber, request ;
     link_list * tmp = (link_list *) args ;
-    //uber = paired_data[0] ;
-    //request = paired_data[1] ;
     uber = tmp[0] ;
     request = tmp[1] ;
-    printf("%s\n", "---------------------");
-    char * str1 = get_path_str(uber.data[0], uber.data[1], request.data[0], request.data[1]);
-    printf("%s\n", str1 );
-    char * str2 = get_path_str(request.data[0], request.data[1], request.data[2], request.data[3]) ;
-    printf("%s\n", str2 );
-    printf("%s\n", "---------------------");
-
-    #if PXA
-	ioctl(lcd_fd, LCD_IOCTL_CLEAR, NULL) ;
-    lcd_write_info_t display ;
-    display.Count = sprintf((char*) display.Msg, str1) ;
-    ioctl(lcd_fd, LCD_IOCTL_WRITE, &display) ;
-	display.Count = sprintf((char*) display.Msg, str2) ;
-	ioctl(lcd_fd, LCD_IOCTL_WRITE, &display) ;
-    #endif
+    int car_id = tmp[0].id ;
+    int * x = malloc(5*sizeof(int)) ;
+    int * y = malloc(5*sizeof(int)) ;
+    // Planning Path
+    x[0] = uber.data[0] ;
+    y[0] = uber.data[1] ;
+    x[1] = uber.data[0] ;
+    y[1] = request.data[1] ;
+    x[2] = request.data[0] ;
+    y[2] = request.data[1] ;
+    x[3] = request.data[0] ;
+    y[3] = request.data[3] ;
+    x[4] = request.data[2] ;
+    y[4] = request.data[3] ;
+    char * target_str = malloc(100*sizeof(char));
+    char * str = malloc(10*sizeof(char));
+    sprintf(target_str, "car %d: %d,%d->%d,%d->%d,%d==>%d,%d",
+        car_id, uber.data[0], uber.data[1], request.data[0], request.data[1], request.data[2], request.data[3], uber.data[0], uber.data[1]);
+    int i ;
+    for (i = 1; i < 5; i++) {
+        sleep(4) ;
+        int next_intersection_id = trans_position(x[i], y[i]) ;
+        int current_intersection_id = trans_position(x[i-1], y[i-1]) ;
+        if (next_intersection_id == current_intersection_id){
+            sprintf(str, "->%d,%d ", x[i], y[i]) ;
+            target_str = strcat( target_str, str ) ;
+            //printf("%s\n", target_str) ;
+            continue ;
+        }
+        while((my_sem_wait(intersection_sem, next_intersection_id, IPC_NOWAIT) < 0)) {
+            target_str = strcat( target_str, "->wait" ) ;
+            //printf("%s\n", target_str) ;
+            #if PXA
+            blink(lcd_fd) ; // 4 seconds
+            #else
+            sleep(4) ;
+            #endif
+        }
+        my_sem_post(intersection_sem, current_intersection_id) ;
+        sprintf(str, "->%d,%d ", x[i], y[i]) ;
+        target_str = strcat( target_str, str ) ;
+        //printf("%s\n", target_str) ;
+    }
+    printf("%s\n", target_str) ;
 
     int * uber_return = malloc(4*sizeof(int)) ;
     uber_return[0] = request.data[2] ;
     uber_return[1] = request.data[3] ;
     uber_return[2] = 0 ;
     uber_return[3] = 0 ;
-    enqueue_safe(&head_uber, uber_return, 0) ;
+    enqueue_safe(&head_uber, uber_return, 0, car_id) ;
     //sem_post(&uber_sem) ;
     my_sem_post(uber_sem, 0) ;
 
-    free(str1) ;
-	free(str2) ;
+    free(target_str) ;
+    free(str) ;
     free(uber_return) ;
     pthread_exit(NULL);
 }
@@ -225,18 +274,19 @@ link_list * dequeue(link_list * head, link_list * result, int idx){
 	}
 }
 
-void enqueue_safe(link_list ** head, int * vals, int priority){
+void enqueue_safe(link_list ** head, int * vals, int priority, int id){
     pthread_mutex_lock(&lock);
-    *head = enqueue(*head, vals, priority) ;
+    *head = enqueue(*head, vals, priority, id) ;
     pthread_mutex_unlock(&lock);
 }
 
-link_list * enqueue(link_list * head, int * vals, int priority){
+link_list * enqueue(link_list * head, int * vals, int priority, int id){
 	link_list * node = (link_list*) malloc(sizeof(link_list)) ;
     int i ;
     for (i = 0; i < 4; i++) {
         node->data[i] = vals[i] ;
     }
+    node->id = id ;
 	node->priority = priority ;
 	node->next = NULL ;
 	if(head == NULL){
@@ -323,16 +373,51 @@ int my_sem_post(int s, int sem_idx){
     }
 }
 
-int my_sem_wait(int s, int sem_idx){
+int my_sem_wait(int s, int sem_idx, int flag){
     struct sembuf sop ;
     sop.sem_num = sem_idx ;
     sop.sem_op = -1 ;
-    sop.sem_flg = 0 ;
+    sop.sem_flg = flag ;
+    //printf("waiting for%d\n", sem_idx);
     if(semop(s, &sop, 1) < 0){
-        printf("%s\n", "ERROR waiting sem");
+        printf("ERROR waiting sem %d, idx=%d\n", s, sem_idx);
         return -1 ;
     }
     else{
         return 0;
     }
 }
+
+#if PXA
+void blink(int led_fd){
+	unsigned short data ;
+	data=LED_ALL_ON ;
+    ioctl (led_fd , LED_IOCTL_SET, &data );
+    //printf("Turn on all LED lamps\n");
+    sleep (1);
+
+    data=LED_ALL_OFF ;
+    ioctl (led_fd , LED_IOCTL_SET, &data );
+    //printf("Turn off all LED lamps\n");
+	sleep(1) ;
+
+	data = LED_ALL_ON ;
+	ioctl (led_fd , LED_IOCTL_SET, &data );
+    sleep (1);
+
+    data=LED_ALL_OFF ;
+    ioctl (led_fd , LED_IOCTL_SET, &data );
+    sleep(1) ;
+}
+
+void show_7seg(int lcd_fd, int number){
+	_7seg_info_t data ;
+	//ioctl(lcd_fd, _7SEG_IOCTL_ON, NULL) ;
+	data.Mode = _7SEG_MODE_PATTERN ;
+	data.Value = seg_data_gen(number) ;
+	data.Which = _7SEG_D5_INDEX | _7SEG_D6_INDEX | _7SEG_D7_INDEX | _7SEG_D8_INDEX ;
+	ioctl(lcd_fd, _7SEG_IOCTL_SET, &data) ;
+	sleep(1) ;
+	//ioctl(lcd_fd, _7SEG_IOCTL_OFF, NULL) ;
+}
+#endif
